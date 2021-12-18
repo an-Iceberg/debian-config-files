ef614b46e1a062919791dfed1ffced1a62fea552
an-Iceberg <a.g.marosi@bluewin.ch>
2021-12-18 18:52:40 +0100

    changed '~' to 'home'

--- install.sh
+++ install.sh
@@ -2,35 +2,35 @@
 
 apt install bat bspwm cmatrix fish g++ gcc git gucharmap htop kitty lolcat neofetch neovim nitrogen picom polybar ranger rofi sxhkd x11-xkb-utils xorg xserver-xorg-input-all xserver-xorg-video-nouveau 
 
-mkdir ~/kazul/.config
-mkdir ~/kazul/.config/bspwm
-mkdir ~/kazul/.config/fish
-mkdir ~/kazul/.config/kitty
-mkdir ~/kazul/.config/neofetch
-mkdir ~/kazul/.config/nvim
-mkdir ~/kazul/.config/picom
-mkdir ~/kazul/.config/polybar
-mkdir ~/kazul/.config/ranger
-mkdir ~/kazul/.config/rofi
-mkdir ~/kazul/.config/sxhkd
+mkdir home/kazul/.config
+mkdir home/kazul/.config/bspwm
+mkdir home/kazul/.config/fish
+mkdir home/kazul/.config/kitty
+mkdir home/kazul/.config/neofetch
+mkdir home/kazul/.config/nvim
+mkdir home/kazul/.config/picom
+mkdir home/kazul/.config/polybar
+mkdir home/kazul/.config/ranger
+mkdir home/kazul/.config/rofi
+mkdir home/kazul/.config/sxhkd
 
-mkdir ~/kazul/Wallpapers
+mkdir home/kazul/Wallpapers
 
-cp .config/bspwm/bspwmrc     ~/kazul/.config/bspwm/bspwmrc
-cp .config/fish/config.fish  ~/kazul/.config/fish/config.fish
-cp .config/kitty/kitty.conf  ~/kazul/.config/bspwm/bspwmrc
-#cp .config/neofetch/         ~/kazul/.config/neofetch/
-cp .config/nvim/init.vim     ~/kazul/.config/nvim/init.vim
-cp .config/picom/picom.conf  ~/kazul/.config/picom/picom.conf
-cp .config/polybar/config    ~/kazul/.config/polybar/config
-cp .config/polybar/launch.sh ~/kazul/.config/polybar/launch.sh
-#cp .config/ranger/           ~/kazul/.config/ranger/
-cp .config/rofi/config       ~/kazul/.config/rofi/config
-cp .config/sxhkd/sxhkdrc     ~/kazul/.config/sxhkd/sxhkdrc
+cp .config/bspwm/bspwmrc     home/kazul/.config/bspwm/bspwmrc
+cp .config/fish/config.fish  home/kazul/.config/fish/config.fish
+cp .config/kitty/kitty.conf  home/kazul/.config/bspwm/bspwmrc
+#cp .config/neofetch/         home/kazul/.config/neofetch/
+cp .config/nvim/init.vim     home/kazul/.config/nvim/init.vim
+cp .config/picom/picom.conf  home/kazul/.config/picom/picom.conf
+cp .config/polybar/config    home/kazul/.config/polybar/config
+cp .config/polybar/launch.sh home/kazul/.config/polybar/launch.sh
+#cp .config/ranger/           home/kazul/.config/ranger/
+cp .config/rofi/config       home/kazul/.config/rofi/config
+cp .config/sxhkd/sxhkdrc     home/kazul/.config/sxhkd/sxhkdrc
 
 cp victormono/* -R /usr/share/fonts/truetype/
 cp victormononf/* -R /usr/share/fonts/truetype/
 
-cp Wallpapers/wallpaper.jpg ~/kazul/Wallpapers/wallpapers.jpg
+cp Wallpapers/wallpaper.jpg home/kazul/Wallpapers/wallpapers.jpg
 
 chsh -s /usr/bin/fish
