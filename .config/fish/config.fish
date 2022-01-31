e5df4c2fc700e483b69eb53ebf63116ed5eca55b
an-Iceberg <a.g.marosi@bluewin.ch>
2022-01-31 18:42:57 +0100

    updated aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,9 +11,9 @@ end
 
 starship init fish | source
 
-alias fcd="cd (find -type d | fzf)"
-alias fnvim="nvim (find -type f | fzf)"
-alias frm="rm -r (find . | fzf -m)"
-alias fshred="shred -zfun3 (find . | fzf -m)"
-alias fcat="cat (find -type f | fzf)"
-alias fless="less (find -type f | fzf)"
+alias fcd="cd (find / -type d | fzf)"
+alias fnvim="nvim (find / -type f | fzf)"
+alias frm="rm -r (find / . | fzf -m)"
+alias fshred="shred -zfun3 (find / . | fzf -m)"
+alias fcat="cat (find / -type f | fzf)"
+alias fless="less (find / -type f | fzf)"
