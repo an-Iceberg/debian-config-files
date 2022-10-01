07de7e7838c605ff7fcdd0d1603497795cfb4d1a
Kazul <82974363+an-Iceberg@users.noreply.github.com>
2022-10-01 14:57:24 +0200

    added btop

--- root-install.sh
+++ root-install.sh
@@ -1,4 +1,4 @@
-apt install curl bspwm cmatrix git feh gucharmap jgmenu htop kitty fish neofetch fastfetch neovim nitrogen picom polybar sxhkd x11-xkb-utils xorg xserver-xorg-input-all xserver-xorg-video-nouveau librust-starship-module-config-derive-dev
+apt install curl bspwm btop cmatrix git feh gucharmap jgmenu htop kitty fish neofetch fastfetch neovim nitrogen picom polybar sxhkd x11-xkb-utils xorg xserver-xorg-input-all xserver-xorg-video-nouveau librust-starship-module-config-derive-dev
 
 cp victormono/* -R /usr/share/fonts/truetype/
 cp victormononf/* -R /usr/share/fonts/truetype/
